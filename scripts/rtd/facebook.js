d35cfdb1b12e2e7c5754ac235bc255fdcf467247
webrtd <web@rtd.dk>
2014-07-07 10:01:29 +0200

    First draft - facebook connect

---
+++ scripts/rtd/facebook.js
@@ -0,0 +1,51 @@
+﻿  function FacebookStatusChangeCallback(response) {
+    console.log('FacebookStatusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      FacebookRTDLogin(response);
+    } else if (response.status === 'not_authorized') {	
+		console.log('Facebook: Not logged into app');
+    } else {
+		console.log('Facebook: Not logged into facebook');
+    }
+  }
+
+  function FacebookCheckLoginState() {
+    FB.getLoginStatus(function(response) {
+      FacebookStatusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '464888166981399',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.0' // use version 2.0
+  });
+
+  FB.getLoginStatus(function(response) {
+    FacebookStatusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  function FacebookRTDLogin(incoming) {
+	alert(incoming.authResponse.accessToken);
+    console.log('Facebook: Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+	alert('Logged in!' + response.email);
+      console.log('Facebook: Successful login for: ' + response.name);
+	  console.log('Facebook: '+response);
+    });
+  }
