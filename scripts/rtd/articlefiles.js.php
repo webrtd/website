02b8189f9568ddc8e33bb39e0958922d904218f2
webrtd <web@rtd.dk>
2014-09-25 10:13:18 +0200

    Fixed automatic listing of article files

--- scripts/rtd/articlefiles.js.php
+++ scripts/rtd/articlefiles.js.php
@@ -1,8 +1,9 @@
 <?
-	require_once '../../config.php';
-	require_once '../../config_terms.php';
-	require_once '../../includes/logic.php';
-	require_once '../../includes/sessionhandler.php';
+	chdir($_SERVER['DOCUMENT_ROOT']);
+	require_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/config_terms.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/includes/logic.php';
+	require_once $_SERVER['DOCUMENT_ROOT'].'/includes/sessionhandler.php';
 	
 	session_start();
 
