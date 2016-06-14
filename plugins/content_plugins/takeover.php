bd70d9b3c772e7e5bcc1f113bde3adb202d80acd
webrtd <web@rtd.dk>
2016-06-14 10:26:52 +0200

    Future role support in transition period

--- plugins/content_plugins/takeover.php
+++ plugins/content_plugins/takeover.php
@@ -33,7 +33,7 @@
 			else 
 			{
 				$_SESSION['user'] = $user;
-				$_SESSION['user']['active_roles'] = fetch_active_roles($user['uid']);
+				$_SESSION['user']['active_roles'] = fetch_active_roles($user['uid'],true);
 				logic_wordpress_synch_user_roles();
 				header("location: /");
 			}
